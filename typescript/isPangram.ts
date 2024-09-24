30bd564e9ebaecfb2df2fa9423d22ef73c49f4a1
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-09-24 19:47:05 +0500

    isPangram: add problem statement at top

--- typescript/isPangram.ts
+++ typescript/isPangram.ts
@@ -1,3 +1,14 @@
+//   ####  Problem statement  ####
+
+// A pangram is a sentence that contains every single letter
+// of the alphabet at least once. For example, the sentence
+// "The quick brown fox jumps over the lazy dog" is a pangram,
+// because it uses the letters A-Z at least once (case is irrelevant).
+
+// Given a string, detect whether or not it is a pangram.
+// Return True if it is, False if not. Ignore numbers and
+// punctuation.
+
 export function isPangram(phrase: string): boolean {
   const dictionary: Record<string, boolean> = {};
   const sanitizedInput = phrase
